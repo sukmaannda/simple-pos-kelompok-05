6909258e083c029a2d44dd0cf4b6543515bd4dd2
RafiZFSIB1C <rafizf.fauzan25@gmail.com>
2026-09-20 15:11:21 +0700

    feat: tambah view riwayat transaksi

---
+++ resources/views/transactions/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title', 'Riwayat Transaksi')
+
+@section('content')
+<div class="p-6">
+    <h1 class="text-xl font-semibold mb-4">Riwayat Transaksi</h1>
+    <div class="bg-white border rounded-md p-4 text-slate-600 shadow-sm">
+        <p>Belum ada riwayat transaksi yang tercatat.</p>
+    </div>
+</div>
+@endsection
